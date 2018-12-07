4de68326f3245380e63a12530dc163b236d23742
luliang <lgh_lul@163.com>
2018-12-07 19:02:27 +0800

    小程序

--- server-answer/src/main/java/com/answer/mapper/QuesTypeMapper.java
+++ server-answer/src/main/java/com/answer/mapper/QuesTypeMapper.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public interface QuesTypeMapper{
   public List<QuesType> queryQuesTypeList();
+
+  public List<QuesType> queryQuesTypeListWithQuesNum(String openID);
   
   public QuesType queryQuesByID(long typeID);
 }
