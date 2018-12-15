eb9e8f9ec6e2ff1728bbe5dfcb4e3e1fc9b911be
luliang <lgh_lul@163.com>
2018-12-15 19:11:56 +0800

    忽略

--- admin-answer/src/main/java/com/answer/ServerApplication.java
+++ admin-answer/src/main/java/com/answer/ServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @MapperScan(basePackages="com.answer.mapper")
@@ -29,5 +30,9 @@ public class ServerApplication extends WebMvcConfigurerAdapter {
 		//添加拦截器
 		//registry.addInterceptor(new SessionInterceptor()).addPathPatterns("/**");
 		super.addInterceptors(registry);
+	}
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
 	}
 }
---
+++ admin-answer/src/main/java/com/answer/controller/PageController.java
@@ -0,0 +1,19 @@
+package com.answer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @CLassName PageController
+ * @Description TODO
+ * @Author ll
+ * @Date 2018/12/15 17:47
+ **/
+@Controller
+public class PageController {
+
+    @RequestMapping("toPage")
+    public String toPage(String path){
+        return path;
+    }
+}
