27aa981e1a6a53d0a9bfa60d4b9d468a04dd5390
卢亮 <lgh_lul@163.com>
2019-12-20 16:33:02 +0800

    fix:谁与争锋导出失败问题修复

--- admin-answer/src/main/java/com/answer/service/impl/TActivityAnswerServiceImpl.java
+++ admin-answer/src/main/java/com/answer/service/impl/TActivityAnswerServiceImpl.java
@@ -25,22 +25,28 @@ public class TActivityAnswerServiceImpl extends BaseServiceImpl<TActivityAnswer>
     @Autowired
     private CacheHelper cacheHelper;
 
-    @Autowired
-    private TActivityMapper activityMapper;
-
     @Override
     public List<UserAnswer4export> listUserAnswer4export(Long activityID) {
         List<UserAnswer4export> userAnswer4exports = activityUserMapper.listUserAnswer4export(activityID);
-        if(userAnswer4exports != null && userAnswer4exports.size() > 0){
+        if (userAnswer4exports != null && userAnswer4exports.size() > 0) {
             ActivityQuery activityQuery = new ActivityQuery();
             activityQuery.setActivityStatus(CommonConstant.Common.ACTIVITY_STATUS_END);
-                for (UserAnswer4export userAnswer4export : userAnswer4exports) {
-                    TUser user = cacheHelper.getUser(userAnswer4export.getOpenid());
-                    TJobnum jobNum = cacheHelper.getJobNum(user.getJobNum());
-                    TOrganization org = cacheHelper.getOrg(jobNum.getOrgID());
-                    userAnswer4export.setRealName(jobNum.getRealName());
-                    userAnswer4export.setOrgName(org.getOrgName());
+            for (int i = userAnswer4exports.size() - 1; i >= 0; i--) {
+                UserAnswer4export userAnswer4export = userAnswer4exports.get(i);
+                TUser user = cacheHelper.getUser(userAnswer4export.getOpenid());
+                if (user == null) {
+                    userAnswer4exports.remove(i);
+                    continue;
                 }
+                if (user.getJobNum() == null) {
+                    userAnswer4exports.remove(i);
+                    continue;
+                }
+                TJobnum jobNum = cacheHelper.getJobNum(user.getJobNum());
+                TOrganization org = cacheHelper.getOrg(jobNum.getOrgID());
+                userAnswer4export.setRealName(jobNum.getRealName());
+                userAnswer4export.setOrgName(org.getOrgName());
+            }
         }
         return userAnswer4exports;
     }
